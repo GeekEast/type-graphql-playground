e18a0f3711a5e4c0aa689052b7d595b5ed3c20e1
James Tan <james@predictivehire.com>
2022-01-23 12:22:32 +1030

    feat(group): add users field to group entity

--- src/modules/group/group.entity.ts
+++ src/modules/group/group.entity.ts
@@ -1,7 +1,8 @@
+import { UserEntity } from './../user/user.entity';
 import { IGroup } from './group.schema';
 import { Field, ID, ObjectType } from 'type-graphql';
 import { Service } from 'typedi';
-import { Expose, plainToInstance, Transform } from 'class-transformer';
+import { Expose, plainToInstance, Transform, Type } from 'class-transformer';
 
 @Service()
 @ObjectType()
@@ -22,6 +23,11 @@ export class GroupEntity {
   @Expose({ toClassOnly: true })
   @Transform(({ obj }) => obj.userCount || 0, { toClassOnly: true })
   userCount: number;
+
+  @Field(() => UserEntity, { nullable: true })
+  @Expose({ toClassOnly: true })
+  @Type(() => UserEntity)
+  users: [UserEntity];
 
   static fromRepoObject(obj: any): GroupEntity {
     if (!obj) return null;
